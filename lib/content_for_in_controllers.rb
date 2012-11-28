d06b5e500e38011d3a3a85b0165a6203d135d67e
Juliana Lucena <julianalucenaa@gmail.com>
2012-11-28 14:28:14 -0300

    Changes module to extend ActiveSupport::Concern.

--- lib/content_for_in_controllers.rb
+++ lib/content_for_in_controllers.rb
@@ -1,22 +1,25 @@
 module ContentForInControllers
-  def view_context
-    super.tap do |view|
-      (@_content_for || {}).each do |name,content|
-        view.content_for name, content
+  extend ActiveSupport::Concern
+  module InstanceMethods
+    def view_context
+      super.tap do |view|
+        (@_content_for || {}).each do |name,content|
+          view.content_for name, content
+        end
       end
     end
-  end
-  def content_for(name, content) # no blocks allowed yet
-    @_content_for ||= {}
-    if @_content_for[name].respond_to?(:<<)
-      @_content_for[name] << content
-    else
-      @_content_for[name] = content
+
+    def content_for(name, content) # no blocks allowed yet
+      @_content_for ||= {}
+      if @_content_for[name].respond_to?(:<<)
+        @_content_for[name] << content
+      else
+        @_content_for[name] = content
+      end
+    end
+
+    def content_for?(name)
+      @_content_for[name].present?
     end
   end
-  def content_for?(name)
-    @_content_for[name].present?
-  end
 end
-
-ActionController::Base.send :include, ContentForInControllers
